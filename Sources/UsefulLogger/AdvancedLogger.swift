61c86124fe131a60fe7ee5ff3aaee57cae3283fd
burak.uzunboy <b_uzunboy@outlook.com>
2020-03-16 16:11:05 +0100

    Small bug fix

--- Sources/UsefulLogger/AdvancedLogger.swift
+++ Sources/UsefulLogger/AdvancedLogger.swift
@@ -136,7 +136,7 @@ public class AdvancedLogger: LoggingDelegate {
         let destPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
         let fullDestPath = NSURL(fileURLWithPath: destPath).appendingPathComponent("\(self.logFileName).log")
         let fullDestPathString = fullDestPath!.path
-        if !FileManager.default.fileExists(atPath: fullDestPathString) {
+        if FileManager.default.fileExists(atPath: fullDestPathString) {
             return FileManager.default.contents(atPath: fullDestPathString)
         }
         
